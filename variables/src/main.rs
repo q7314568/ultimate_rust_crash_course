5140890f9d55b323e00d2dec6ea03ee76ea8a0af
q7314568 <q7314568@gmail.com>
2024-09-11 14:31:42 +0800

    完成part 2及額外嘗試

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let missiles=8;
-    let ready=2;
-
+    const STARTING_MISSILES:i32=8;
+    const READY_AMOUNT:i32=2;
+    let (missiles,ready)=(STARTING_MISSILES,READY_AMOUNT);
     println!("Firing {} of my {} missiles",ready,missiles);
-    
+    // missiles=missiles-ready;
+    println!("{} missiles left",missiles-ready);
 }
