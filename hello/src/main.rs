b6764cb3fdef961e8c59d4e3189425d10924d0f6
q7314568 <q7314568@gmail.com>
2024-09-11 16:53:20 +0800

    finish exercise c

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+
+    let small_array: [i32; 32] = [0; 32]; // 可以自動實現 Debug
+    println!("{:?}", small_array); // 這是合法的
+
+    let large_array: [i32; 33] = [0; 33]; // 超過 32 的限制
+    println!("{:?}", large_array); // 編譯錯誤，因為無法自動實現 Debug
 }
